26fe4cf14e32d5c5fcd413f9a13077eee1ba88ad
theycallmemorty <michaeldouglasmorton@gmail.com>
2012-05-25 21:57:09 -0400

    Update to Euro 2012 Layout

--- EUROgenerator/generate.py
+++ EUROgenerator/generate.py
@@ -90,7 +90,43 @@ print """<!DOCTYPE html>
 <script type="text/javascript" src="euro2012.js"></script>
 <link rel="stylesheet" href="euro2012.css" type="text/css" />
 </head>
-<body>"""
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+<div class="left">
+
+<h1>2012 UEFA Euro Cup Results Calculator</h1>
+<div>
+Enter some scores and the standings will update automatically.  Easily determine what it will take for your team to advance.<br />
+<br />
+Feedback: <a href="http://www.twitter.com/theycallmemorty">@theycallmemorty</a>
+</div>
+
+         <div>
+<!--
+            <div id="tweet">
+             <a href="https://twitter.com/share" class="twitter-share-button" data-via="theycallmemorty">Tweet</a>
+             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>  
+            </div>
+            <div id="facebook" class="fb-like" data-send="false" data-layout="button_count" data-width="75" data-show-faces="true"></div>
+-->
+         </div>
+
+     <div class="bottom"><div id="adholder">
+        My Ad
+     </div></div>
+
+</div>
+<div class="right">
+"""
 for key in keys:
     teams = list()
     teams.extend(groupmatches[key][0])
@@ -150,6 +186,8 @@ print"""
 <div class="champ">
   <span></span>
 </div>
+</div>
+
 </div>
 </body>
 </html>"""
