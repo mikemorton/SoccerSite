c74e097d383f61c2f0c2c2171fb9897393173d25
theycallmemorty <michaeldouglasmorton@gmail.com>
2013-05-18 09:56:20 -0400

    AddResult testing

--- test/wctesting.js
+++ test/wctesting.js
@@ -28,6 +28,18 @@ test("Team.AddResult", function () {
     equal(elem.nGA, 5, elem.sName + " 5 goals against");
     equal(elem.nGD, 0, elem.sName + " 0 goal differential");
 
+    elem.AddResult(4,3);
+    elem.AddResult(1,1);
+    elem.AddResult(0,2);
+
+    equal(elem.nGP, 6, elem.sName + " 6 games played");
+    equal(elem.nP, 8, elem.sName + " 8 points");
+    equal(elem.nW, 2, elem.sName + " 2 wins");
+    equal(elem.nD, 2, elem.sName + " 2 draws");
+    equal(elem.nL, 2, elem.sName + " 2 losses");
+    equal(elem.nGF, 10, elem.sName + " 10 goals scored");
+    equal(elem.nGA, 11, elem.sName + " 11 goals against");
+    equal(elem.nGD, -1, elem.sName + " -1 goal differential");  
 });
 
 test("Basic Group Results Testing", function () {
