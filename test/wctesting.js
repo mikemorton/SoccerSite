b4668f8b9d73436e15d1ec1c1704f0cae4f7c206
theycallmemorty <michaeldouglasmorton@gmail.com>
2013-05-18 09:48:15 -0400

    Add Team object, GetTeamStats function and unit tests

--- test/wctesting.js
+++ test/wctesting.js
@@ -1,3 +1,58 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+
+test("Team Object Initialization", function () {
+    var elem = new Team('mike');
+    ok(elem.sName == 'mike');
+    ok(elem.nGP == 0, elem.sName + " 0 games played");
+    ok(elem.nP == 0, elem.sName + " 0 points");
+    ok(elem.nW == 0, elem.sName + " 0 wins");
+    ok(elem.nD == 0, elem.sName + " 0 draws");
+    ok(elem.nL == 0, elem.sName + " 0 losses");
+    ok(elem.nGF == 0, elem.sName + " 0 goals scored");
+    ok(elem.nGA == 0, elem.sName + " 0 goals against");
+    ok(elem.nGD == 0, elem.sName + " 0 goal differential");
+});
+
+test("Team.AddResult", function () {
+    var elem = new Team('mike');
+
+    elem.AddResult(5,0);
+    elem.AddResult(0,0);
+    elem.AddResult(0,5);
+
+    ok(elem.nGP == 0, elem.sName + " 3 games played");
+    ok(elem.nP == 0, elem.sName + " 4 points");
+    ok(elem.nW == 0, elem.sName + " 1 wins");
+    ok(elem.nD == 0, elem.sName + " 1 draws");
+    ok(elem.nL == 0, elem.sName + " 1 losses");
+    ok(elem.nGF == 0, elem.sName + " 5 goals scored");
+    ok(elem.nGA == 0, elem.sName + " 5 goals against");
+    ok(elem.nGD == 0, elem.sName + " 0 goal differential");
+
+});
+
+test("Basic Group Results Testing", function () {
+    var aMatches = [];
+    aMatches[aMatches.length] = {homeTeam: 'Serbia', homeScore: 0, awayTeam: 'Ghana', awayScore: 0};
+    aMatches[aMatches.length] = {homeTeam: 'Germany', homeScore: 0, awayTeam: 'Australia', awayScore: 0};
+
+    aMatches[aMatches.length] = {homeTeam: 'Germany', homeScore: 0, awayTeam: 'Serbia', awayScore: 0};
+    aMatches[aMatches.length] = {homeTeam: 'Ghana', homeScore: 0, awayTeam: 'Australia', awayScore: 0};
+
+    aMatches[aMatches.length] = {homeTeam: 'Ghana', homeScore: 0, awayTeam: 'Germany', awayScore: 0};
+    aMatches[aMatches.length] = {homeTeam: 'Australia', homeScore: 0, awayTeam: 'Serbia', awayScore: 0};
+
+    var aRet = GetTeamStats(aMatches);
+    ok(aRet.length == 4, "4 teams returned");
+    ok(_.uniq(_.pluck(aRet, 'sName')).length == 4, "4 distinct teams returned");
+    
+    _.each(aRet, function (elem) {
+	ok(elem.nGP == 3, elem.sName + " 3 games played");
+	ok(elem.nP == 3, elem.sName + " 3 points");
+	ok(elem.nW == 0, elem.sName + " 0 wins");
+	ok(elem.nD == 3, elem.sName + " 3 draws");
+	ok(elem.nL == 0, elem.sName + " 0 losses");
+	ok(elem.nGF == 0, elem.sName + " 0 goals scored");
+	ok(elem.nGA == 0, elem.sName + " 0 goals against");
+	ok(elem.nGD == 0, elem.sName + " 0 goal differential");
+    });
 });
--- wc2014.js
+++ wc2014.js
@@ -1,3 +1,51 @@
+function Team(name) {
+
+    this.sName = name;
+    this.nGP = 0;
+    this.nP = 0;
+    this.nW = 0;
+    this.nD = 0;
+    this.nL = 0;
+    this.nGF = 0;
+    this.nGA = 0;
+    this.nGD = 0;
+
+    this.AddResult = function (nScored, nAgainst) {
+	this.nGP++;
+	this.nGF = this.nGF + nScored;
+	this.nGA = this.nGA + nAgainst;
+
+	this.nGD = this.nGF - this.nGA;
+
+	if(nScored > nAgainst)
+	    this.nW++;
+	else if(nScored == nAgainst)
+	    this.nD++;
+	else
+	    this.nL++;
+
+	this.nP = this.nW * 3 + this.nD;
+    }
+}
+
+function GetTeamStats(aMatches) {
+    // Get list of teams
+    var aTeams = _.map(_.uniq(_.pluck(aMatches, 'homeTeam')), function (name) {
+	return new Team(name);
+    });
+
+    var mTeams = _.object(_.map(aTeams, function(oTeam) {
+	return [oTeam.sName, oTeam]
+    }));
+
+    _.each(aMatches, function(oMatch) {
+	mTeams[oMatch.homeTeam].AddResult(oMatch.homeScore, oMatch.awayScore);
+	mTeams[oMatch.awayTeam].AddResult(oMatch.awayScore, oMatch.homeScore);
+    });
+
+    return aTeams;
+}
+
 function UpdateGroup(sGroupID)
 { 
   var bHasEmptyMatch = false;
